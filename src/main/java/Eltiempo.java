252c04b7de641b6eda2e1666c3136bbb181a904b
ndiazdossantos <ndiazdossantos@danielcastelao.org>
2020-02-05 14:54:50 +0100

    Añadido titulo y estado

--- src/main/java/Eltiempo.java
+++ src/main/java/Eltiempo.java
@@ -9,9 +9,15 @@ public class Eltiempo {
     public static void main(String[] args) throws IOException {
 
         Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").userAgent("Mozilla").get();
+        String titulo= doc.title();
         Elements miElemento = doc.getElementsByClass("today_nowcard-temp");
         String miTexto = miElemento.text();
+        Elements miElemento1 = doc.getElementsByClass("today_nowcard-phrase");
+        String miTexto1 = miElemento1.text();
+        System.out.println(titulo);
         System.out.println(miElemento);
+        System.out.println(miElemento1);
+
 
     }
 }
