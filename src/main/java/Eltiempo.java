58dee014915caed525f073a3e18571192fd886bb
ndiazdossantos <ndiazdossantos@danielcastelao.org>
2020-02-05 14:49:06 +0100

    Terminado buscar temperatura

--- src/main/java/Eltiempo.java
+++ src/main/java/Eltiempo.java
@@ -1,6 +1,6 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 import java.io.IOException;
@@ -8,10 +8,11 @@ import java.io.IOException;
 public class Eltiempo {
     public static void main(String[] args) throws IOException {
 
-        Document doc = Jsoup.connect("http://stackoverflow.com").userAgent("Mozilla").get();
-        Element miElemento = doc.select("").first();
+        Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").userAgent("Mozilla").get();
+        Elements miElemento = doc.getElementsByClass("today_nowcard-temp");
         String miTexto = miElemento.text();
         System.out.println(miElemento);
+
     }
 }
 
